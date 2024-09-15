97d847d08bb0beaa9adea1737f2fd5d85fab255a
ardaefeozcan <ardaefe.ozcan@hisarschool.k12.tr>
2024-09-15 23:30:59 +0300

    Kod eklendi

--- LearningSwift_Day1(Example 0).playground/Contents.swift
+++ LearningSwift_Day1(Example 0).playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+var greeting = "Hello "
 
-var greeting = "Hello, playground"
+let pi=3.14
+let pi1:Float=3.14
+
+var bosluk:String
+let bosSabit:Int
+print(greeting)
+let isim = "Beyza"
+print(greeting + isim)
+print("Hello,\(isim) ")
+print("\(greeting)\(isim)")
+
+
+var yaslar = [3,5,7,11,10]
+var isimler = ["Ali", "Mehmet", "Ayşe"]
+print("\(isimler[0]), \(yaslar[4])")
